74f8830c0e90072c0dfcd505a519229a3c3633ac
wanderer-ing <jsjune1222@gamil.com>
2026-05-21 11:03:43 +0900

    modified

--- code/2-2.c
+++ code/2-2.c
@@ -5,13 +5,13 @@ enum Color { RED, GREEN, BLUE };
 enum Status { OK = 0, ERR = -1, BUSY = 1 };
 
 int main(void) {
-    printf("RED    = %d\n", RED);
-    printf("GREEN  = %d\n", GREEN);
-    printf("BLUE   = %d\n\n", BLUE);
+    printf("RED   = %d\n", RED);
+    printf("GREEN = %d\n", GREEN);
+    printf("BLUE  = %d\n\n", BLUE);
 
-    printf("OK     = %d\n", OK);
-    printf("ERR    = %d\n", ERR);
-    printf("BUSY   = %d\n", BUSY);
+    printf("OK    = %d\n", OK);
+    printf("ERR   = %d\n", ERR);
+    printf("BUSY  = %d\n", BUSY);
 
     return 0;
 }
